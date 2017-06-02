35c92ce60751efcc555d8129208a5d94f008d31b
tingxuanz <zhangtx93@gmail.com>
2017-06-02 15:03:32 +1000

    test Twitter

--- cloud/main.js
+++ cloud/main.js
@@ -14,7 +14,7 @@ Parse.Cloud.afterSave('RealtimeStatus', function(request) {
   var T = new Twit({
   consumer_key:         '8A5ftN1w5QPsHvEqCjYqOtiXo',
   consumer_secret:      'KeO62KUcFemMazCCRTbCZwT5StoaaFPhuGq2FaCG6KLamqKVJl',
-  access_token:         '	810308473732771840-Hx5lzVsBWncktOERXv3x2ZuHao5DizC',
+  access_token:         '810308473732771840-Hx5lzVsBWncktOERXv3x2ZuHao5DizC',
   access_token_secret:  'NverWhaMVEYzjVUbt4esVjqC2R91sQnQrHP771WMpLaPM',
   timeout_ms:           60*1000,  // optional HTTP request timeout to apply to all requests.
 })
